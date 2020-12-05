b63e658b1068909222a08dc8b7beac31d4a193f3
Eberson Ramos <ebersonramos11@gmail.com>
2020-12-05 13:31:34 -0300

    [shell] Refatoração e adicionado as validações e download do gradle.

--- env-dev.sh
+++ env-dev.sh
@@ -2,7 +2,7 @@
 
 path_common=$(pwd)
 
-if [ -e "${path_common}"/lib/common.sh ];then
+if [ -e "${path_common}"/lib/common.sh ]; then
 	. ${path_common}/lib/common.sh $1
 else
 	. /usr/local/bin/lib/common.sh $1
@@ -15,9 +15,9 @@ echo "## Configuration initial the of environment for developer ##"
 echo "${espace_line}"
 
 if [ -z "$JAVA_VERSION" ]; then
-	get_text_config "1) Config Default JDK/JRE"
+	getTextConfig "1) Config Default JDK/JRE"
 else
-	get_text_config "1) Config JDK/JRE Version $JAVA_VERSION"
+	getTextConfig "1) Config JDK/JRE Version $JAVA_VERSION"
 fi
 
 echo "## Valid if Default JDK/JRE exist in env JAVA_HOME ##"
@@ -41,8 +41,8 @@ if [ -z "$JAVA_HOME" ]; then
 			error_code=$?
 		fi
 
-		if [ "$error_code" -ne "0" ];then
-			echo "${reset}## ${red}${bold}Error: ${reset}Failed to executed shell script ${green}${bold}dell.sh ${reset}##"
+		if [ "$error_code" -ne "0" ]; then
+			logError "Failed to executed shell script ${green}${bold}dell.sh ${reset}"
 			exit $error_code
 		fi
 
@@ -53,16 +53,16 @@ if [ -z "$JAVA_HOME" ]; then
 			echo "## Install Default JDK/JRE"
 			echo "${espace_line}"
 			
-			install_default_jdk_jre "${password_file}"
+			installDefaultJdkJre "${password_file}"
 
-			get_java_path
+			getJavaPath
 		else
 			echo "## Install JDK/JRE Version $JAVA_VERSION"
 			echo "${espace_line}"
 			
-			install_openjdk_jre_by_version "${password_file}"
+			installOpenjdkJreByVersion "${password_file}"
 
-			get_java_path
+			getJavaPath
 		fi
 
 		echo "## Config JAVA_HOME in ${bold}/etc/environment ${reset}##"
@@ -73,14 +73,14 @@ if [ -z "$JAVA_HOME" ]; then
 			echo "## Directory ${bold}${JAVA_PATH} ${reset}exists. ##"
 			echo "${espace_line}"
 
-			add_java_home_in_file_environment "${password_file}"
+			addJavaHomeInFileEnvironment "${password_file}"
 
 			echo "## Add Default JDK/JRE `echo $JAVA_HOME` ##"
 			echo "${espace_line}"
 		else
-			echo "## ${red}${bold}Error: ${reset}Directory ${JAVA_PATH} does not exists. ##"
+			logError "Directory ${JAVA_PATH} does not exists."
 			echo "${espace_line}"
-			exit 100
+			exit 1
 		fi
 else
 	echo "${yellow}${bold}${format_line}"
@@ -95,11 +95,48 @@ else
 	echo "${espace_line}"
 fi
 
-get_text_config "End 1) Config Default JDK/JRE"
+getTextConfig "End 1) Config Default JDK/JRE"
 
-get_text_config "2) Config Gradle"
+getTextConfig "2) Config Gradle"
 
-get_text_config "End 2) Config Gradle"
+# force to lowercase
+typeset -l answer="i"
+
+while [[ "$answer" != "y" && "$answer" != "n" ]]
+do
+ echo -n "${bold}## Do you have a specific version?${reset} ${red}[y] or [n]${reset}${bold}.:${reset} "
+ IFS= read answer
+done
+
+typeset -l user_answer=$answer
+
+if [ "$user_answer" == "y" ]; then	
+	echo -n "${bold}## What's your version?${reset} ${red}Ex: v4.10.5${reset}${bold}.: v${reset}" 
+        read version
+        gradle_version=$version
+
+	validFormatVersion $gradle_version
+
+        getTextConfig "Download specific version gradle.: gradle-${gradle_version}"
+else
+	getVersionGradle
+	gradle_version="$GRADLE_VERSION"
+	
+	getTextConfig "Download last version gradle.: gradle-${gradle_version}"
+fi	
+
+#Valida se ja existe
+if [[ -d /opt/gradle || -d /opt/gradle/gradle-${gradle_version} ]]; then
+	#Mostra uma mensagem que já existe uma versao configurada
+	getTextConfig "End 2) Config Gradle"
+
+else
+	getTextConfig "Init Download Gradle.: gradle-${gradle_version}"
+	#Faz o download da versao informada
+	downloadGradleZipFile $gradle_version
+
+	#Apos o download adiciona no /etc/enviroment
+fi
 
 echo "${magenta}${bold}## End config env dev. Bye, ${USER^}! by `uname --kernel-name`/`uname --nodename` ;) ## ${reset}"
 exit 0
--- lib/common.sh
+++ lib/common.sh
@@ -30,12 +30,14 @@ magenta=`tput setaf 5`
 #################
 JAVA_VERSION=$1
 JAVA_PATH="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64"
+GRADLE_VERSION=""
+DOWNLOAD_DIR="/home/$USER/Downloads"
 
 #######################
 ## Utils's Functions ##
 #######################
 
-get_java_path() {
+getJavaPath() {
  echo "${magenta}${bold}${format_line}"
  echo "## Init get_java_path()"
  echo "${espace_line}"
@@ -46,27 +48,85 @@ get_java_path() {
  echo "${format_line}${reset}"
 }
 
-get_text_config() {
+getTextConfig() {
  echo "${magenta}${bold}${format_line}"
  echo "## $1 ##"
  echo "${format_line}${reset}"
  echo "${espace_line}"
 }
 
-install_default_jdk_jre() {
+installDefaultJdkJre() {
  echo "## Init install_default_jdk_jre()"
  echo "$1" | sudo -S -v && sudo apt install default-jre --yes && sudo apt install default-jdk --yes && echo "JAVAC VERSION: `javac -version`" && echo "JAVA VERSION: `java -version`"
  echo "## End install_default_jdk_jre()"
 }
 
-install_openjdk_jre_by_version() {
+installOpenjdkJreByVersion() {
  echo "## Init install_openjdk_jre_by_version"
  echo "$1" | sudo -S -v && sudo apt install openjdk-${JAVA_VERSION}-jre --yes && sudo apt install openjdk-${JAVA_VERSION}-jdk --yes && echo "JAVAC VERSION: `javac -version`" && echo "JAVA VERSION: `java -version`"
  echo "## End install_openjdk_jre_by_version"
 }
 
-add_java_home_in_file_environment(){
+addJavaHomeInFileEnvironment(){
  echo "## Int add_java_home_in_file_environment()"
  echo "$1" | sudo -S -v && sudo sed -i -e '$aJAVA_HOME="'${JAVA_PATH}'"' /etc/environment && source /etc/environment
  echo "## End add_java_home_in_file_environment()"
+}
+
+validFormatVersion() {
+ 
+ local version=$1
+ local text_version="[ ${bold}${green}OK${reset} ] Version valid formart v"
+
+ if [[ $version == ?.?.? ]]; then
+ 	logInfo "${text_version}${version}"
+ elif [[ $version == ?.?? ]]; then
+ 	logInfo "${text_version}${version}"
+ elif [[ $version == ?.??.? ]]; then
+	logInfo "${text_version}${version}"
+ elif [[ $version == ?.?? ]]; then
+	logInfo "${text_version}${version}"
+ elif [[ $version == ?.? ]]; then
+	logInfo "${text_version}${version}"
+ else
+	echo "${espace_line}"
+ 	logInfo "[ ${bold}${red}x${reset} ] Version invalid format v$version"
+	exit 1
+ fi
+}
+
+logInfo(){
+ local message=$1
+ echo "${espace_line}"
+ echo "## $message"
+}
+
+logError(){
+ local message=$1
+ echo "${bold}${red}### Error.:${reset} $message"
+}
+
+getVersionGradle(){
+
+ if [ -z "$GRADLE_VERSION" ]; then	 
+  logInfo "GET Last Gradle Version......"
+  curl -s https://gradle.org/releases/ | grep -o "<span>v[^>]*</span>" | tr -d '<span/>' > $(pwd)/versions.txt
+  logInfo "End GET Last Gradle Version......."
+ fi
+
+ while [[ "$GRADLE_VERSION" == "" ]]
+ do 
+	 IFS= read -r line
+	 logInfo "Last Gradle Version.: $line"
+	 if [ $line != "" ]; then
+		line=$(echo "${line//v/}")
+	 	GRADLE_VERSION=$line
+	 fi
+
+ done < "$(pwd)/versions.txt"
+}
+
+downloadGradleZipFile(){
+ logInfo "Saved in $DOWNLOAD_DIR"
+ wget https://services.gradle.org/distributions/gradle-"$1"-all.zip -P "$DOWNLOAD_DIR"
 }
